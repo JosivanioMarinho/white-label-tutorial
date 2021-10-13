b33a2fc3448bbe990f54e0af99e671b668c97c02
JosivanioMarinho <josivaniom69@gmail.com>
2021-10-13 16:06:15 -0300

    Aulas 08 e 09

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/AddProductViewModel.kt
+++
@@ -1,7 +0,0 @@
-package br.com.douglasmotta.whitelabeltutorial.ui.products
-
-import androidx.lifecycle.ViewModel
-
-class AddProductViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
